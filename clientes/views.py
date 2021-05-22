43e79b78c2ce2bff68e0f2a1f381be2ca811d71b
Izaiasjun1Dev <solucaoprogramer@gmail.com>
2021-05-22 00:23:42 -0300

    implent authentication to endpoint customer

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
@@ -15,3 +18,13 @@ class ClientesViewSet(viewsets.ModelViewSet):
     ordering_fields = ['nome']
     search_fields = ['nome', 'cpf']
     filterset_fields = ['ativo']
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return Response(content)
